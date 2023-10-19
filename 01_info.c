0922f7705371abb914aebaee39e3a137e06fa94c
pansuriyarahul107 <pansuriyarahul107.com>
2023-10-18 19:12:33 -0800

    Parth

---
+++ 01_info.c
@@ -0,0 +1,16 @@
+#incṇlude<stdio.h>
+
+int main(){
+int i,j,k;
+printf("enter array a[%d][%d][%d]:",i,j,k);
+
+for (i=0;i<=2;i++){
+   for (j=0;j<=3;j++){
+    for (k=0;k<=3;k++){
+        printf ("enter array\n");
+        scanf ("%d",a[i][j][k]);
+    }
+   }
+}
+printf ("\n");
+}
